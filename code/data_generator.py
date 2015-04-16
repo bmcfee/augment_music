9bbe4de26d9102d26c4d5fc9ee0aae6f56b40c06
Brian McFee <brian.mcfee@nyu.edu>
2015-04-16 15:31:38 -0400

    logscaled X

--- code/data_generator.py
+++ code/data_generator.py
@@ -20,106 +20,6 @@ def time_to_frames(times, hop_length=512, sr=22050):
     return (np.atleast_1d(times) * float(sr) / hop_length).astype(int)
 
 
-def intersect_labels(annotation, time, duration, overlap):
-    '''Slice a JAMS annotation frame down to an observation window.
-
-    Parameters
-    ----------
-    annotation : jams.JamsFrame
-
-    time : float > 0
-    duration : float > 0
-        The window, in seconds
-
-    overlap : float > 0
-        The minimum amount of (total) overlap time necessary
-        to retain the label
-
-    Returns
-    -------
-    labels : list
-        A list of values corresponding to matching annotations
-    '''
-
-    a_trim = annotation.copy()
-
-    # Clip intervals to the target
-    a_trim['time'] = annotation['time'].clip(lower=pd.to_timedelta(time,
-                                                                   unit='s'))
-    a_trim['end'] = (annotation['time'] +
-                     annotation['duration']).clip(upper=pd.to_timedelta(time +
-                                                                        duration,
-                                                                        unit='s'))
-
-    a_trim['duration'] = a_trim['end'] - a_trim['time']
-
-    # Select out the non-empty intervals
-    a_trim = a_trim[a_trim['duration'] > 0]
-
-    # If there are no labeled intervals left, return an empty list
-    if not len(a_trim):
-        return []
-
-    # Aggregate time by label
-    label_time = a_trim.groupby('value')['duration'].sum()
-
-    # Keep only those that span enough time
-    label_time = label_time[label_time >= pd.to_timedelta(overlap, unit='s')]
-
-    return list(label_time.index)
-
-
-def _generate_data(name, data_path, label_encoder,
-                  n_columns=128, min_overlap=0.25):
-    '''Data generator for a single track
-
-    Parameters
-    ----------
-    name : str
-        The name of the track, eg, 'MusicDelta_Reggae_000081'
-
-    data_path : str
-        Path to the directory containing feature and annotation files
-
-    label_encoder : sklearn.preprocessing.MultiLabelBinarizer
-        Transform label arrays into binary vectors
-
-    n_columns : int > 0
-        The width of patches to sample
-
-    min_overlap : float > 0
-        The minimum overlap required for a valid observation
-    '''
-
-    featurefile = os.path.join(data_path, '{}.npz'.format(name))
-
-    X = np.load(featurefile)['C']
-
-    jamfile = os.path.join(data_path, '{}.jams'.format(name))
-
-    jam = jams.load(jamfile, validate=False)
-
-    annotation = jam.annotations[0].data
-
-    annotation = annotation[annotation['value'].isin(label_encoder.classes_)]
-
-    duration = frames_to_time(n_columns)[0]
-    n_total = X.shape[1]
-
-    while len(annotation):
-        # Slice a patch
-        idx = np.random.randint(0, n_total - n_columns)
-
-        # Slice the labels
-        y = intersect_labels(annotation,
-                             frames_to_time(idx)[0],
-                             duration,
-                             min_overlap)
-
-        yield dict(X=X[:, idx:idx+n_columns].T[np.newaxis, np.newaxis],
-                   Y=label_encoder.transform([y]))
-
-
 def make_label_matrix(n, ann, label_encoder):
     '''Generate a binary matrix of labels, sampled to match X
 
@@ -197,7 +97,7 @@ def generate_data(name, data_path, label_encoder,
         # Slice a patch
         idx = np.random.randint(0, n_total - n_columns)
 
-        Xsamp = X[:, idx:idx+n_columns].T
+        Xsamp = np.log1p(X[:, idx:idx+n_columns].T)
         Ysamp = (Y[:, idx:idx+n_columns].T.sum(axis=0, keepdims=True) >=
                  n_overlap)
         yield dict(X=Xsamp[np.newaxis, np.newaxis],
